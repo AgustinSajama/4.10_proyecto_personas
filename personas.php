20407b44255c469c9d5fa1f7312cf88eede38286
AgustinSajama <sajamaagustin2007@gmail.com>
2025-02-24 22:57:05 -0300

    generacion de la tabla HTML

--- personas.php
+++ personas.php
@@ -6,3 +6,19 @@ $consulta = "SELECT * FROM personas;";
 $r = mysqli_query($connection, $consulta);
 
 if ($r && mysqli_num_rows($r) > 0) {
+    echo "<table border='1'>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>DNI</th>
+        </tr>";
+
+while ($fila = mysqli_fetch_assoc($r)) {
+        echo "<tr>
+                <td>{$fila['ID']}</td>
+                <td>{$fila['nombre']}</td>
+                <td>{$fila['DNI']}</td>
+              </tr>";
+        }
+
+        echo "</table>";
