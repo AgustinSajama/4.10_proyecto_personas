c096531b2718e2a4e1b771fd332ac31de36f87ab
AgustinSajama <sajamaagustin2007@gmail.com>
2025-02-24 22:51:48 -0300

    verificando si la consulta fue exitosa y si se encontraron filas en el resultado

--- personas.php
+++ personas.php
@@ -4,3 +4,5 @@ $connection = mysqli_connect("localhost", "root", "", "informacion_personas");
 $consulta = "SELECT * FROM personas;";
 
 $r = mysqli_query($connection, $consulta);
+
+if ($r && mysqli_num_rows($r) > 0) {
